4fcbc58ce5a636812f045915b97d3a1644ca93c6
eduardopianelli <virgilioeduardo500@gmail.com>
2026-03-10 14:35:48 -0300

    add cors e tirando espaços e "icones" indesejados

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,24 +1,19 @@
 const express = require("express");
-
+const cors = require("cors");
 const sensorRoutes  = require("./routes/SensorRoutes");
 const swaggerUI     = require("swagger-ui-express");
 const swaggerSpec   = require("./docs/Swagger");
 
 const app = express();
 
-// middleware para ler JSON
 app.use(express.json());
-
 app.use("/api", sensorRoutes);
-
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-//Iniciar Servidor
 const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
-
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-    console.log(`📚 Swagger em http://localhost:${PORT}/docs`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Swagger em http://localhost:${PORT}/docs`);
 })
